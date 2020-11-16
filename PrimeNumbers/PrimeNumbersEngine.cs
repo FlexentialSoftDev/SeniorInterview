76c6778594bd510b6218f3f468afb702dcca3bda
FlexentialSoftDev <74556219+FlexentialSoftDev@users.noreply.github.com>
2020-11-16 12:06:16 -0700

    Add files via upload

---
+++ PrimeNumbers/PrimeNumbersEngine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrimeNumbers
+{
+	public static class PrimeNumbersEngine
+	{
+
+		public static List<int> GetAllUnder(int max)
+		{
+			List<int> primeNumbers = new List<int>();
+
+			//  Begin Implementation
+			//  End Implementation
+
+			return primeNumbers;
+		}
+	}
+}
